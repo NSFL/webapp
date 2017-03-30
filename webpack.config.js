13dfead63bc8f1e2e71b4149ff9a254645b6161d
Yasser Amr <yh@goodsense.jp>
2017-03-30 14:06:52 +0200

    MISCELLANEOUS: Updating package and webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,20 @@
 var path = require('path');
 module.exports = {
-    entry: './index.js',
-    output: {
-        path: path.resolve(__dirname),
-        path: __dirname,
-        filename: 'bundle.js'
-    },
-    module: {
-        loaders: [
-            { 
-              test: path.join(__dirname ),
-              loader: 'babel-loader',
-              query: {
-                "presets": ["es2015"]
-              },
-            }
-        ]
-    }
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname),
+    path: __dirname,
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      { 
+        test: path.join(__dirname),
+        loader: 'babel-loader',
+        query: {
+          "presets": ["es2015"]
+        },
+      }
+    ]
+  }
 };
